02b1049eda21ab8c0942206b3c4d78f7a38c9d24
Tashmin Mishra <49580316+tashmingulu@users.noreply.github.com>
2021-11-01 13:40:35 +0530

    Update

--- HyundaiEon_NodeMCU.ino
+++ HyundaiEon_NodeMCU.ino
@@ -1,18 +1,21 @@
-
+/*
+ * Car Central Lock system control using NodeMCU
+ * By: Tashmin Mishra
+ */
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
 
 /* Put your SSID & Password */
-const char* ssid = "Hyundai Eon";  // Enter SSID here
-const char* password = "Tashm!n_Eon";  //Enter Password here
+const char* ssid = "";  // Enter SSID here
+const char* password = "";  //Enter Password here
 
 ESP8266WebServer server(80);
 
-uint8_t Relay1 = D3;
-uint8_t Relay2 = D2;
-//uint8_t Relay3 = D2;
-//uint8_t Relay4 = D3;
+uint8_t Relay1 = D5;
+uint8_t Relay2 = D6;
+uint8_t Relay3 = D7;
+uint8_t Relay4 = D8;
 
 int load1, load2, load3, load4;
 
@@ -35,16 +38,15 @@ void setup() {
 
   pinMode(Relay1, OUTPUT); 
   pinMode(Relay2, OUTPUT);
-  digitalWrite(Relay1, 0); 
-//  pinMode(Relay3, OUTPUT);
-//  pinMode(Relay4, OUTPUT); 
-
+  pinMode(Relay3, OUTPUT);
+  pinMode(Relay4, OUTPUT); 
+ 
   EEPROM.begin(512);
   
-  load1 = EEPROM.read(1);
-  load2 = EEPROM.read(2);
-  load3 = EEPROM.read(3);
-  load4 = EEPROM.read(4);
+  load1 =0;
+  load2 =0;
+  load3 =0;
+  load4 =0;
 
   Serial.print(load1); Serial.print(" "); 
   Serial.print(load2); Serial.print(" "); 
@@ -53,8 +55,8 @@ void setup() {
 
   digitalWrite(Relay1, load1); 
   digitalWrite(Relay2, load2); 
-//  digitalWrite(Relay3, load3); 
-//  digitalWrite(Relay4, load4);
+  digitalWrite(Relay3, load3); 
+  digitalWrite(Relay4, load4);
 
   WiFi.softAP(ssid, password);
   IPAddress apip = WiFi.softAPIP();
@@ -65,13 +67,9 @@ void setup() {
   
   server.on("/", handle_OnConnect);
   server.on("/L1on",  handle_L1on);
-  server.on("/L1off", handle_L1off);
   server.on("/L2on",  handle_L2on);
-  server.on("/L2off", handle_L2off);
   server.on("/L3on",  handle_L3on);
-  server.on("/L3off", handle_L3off);
   server.on("/L4on",  handle_L4on);
-  server.on("/L4off", handle_L4off);
   server.onNotFound(handle_NotFound);
   
   server.begin();
@@ -79,29 +77,28 @@ void setup() {
 }
 void loop() {
   server.handleClient();
-
-  digitalWrite(Relay1, !load1); 
-  digitalWrite(Relay2, load2); 
- // digitalWrite(Relay3, load3); 
-//  digitalWrite(Relay4, load4);
-
+    Serial.print(load1); Serial.print(" "); 
+    Serial.print(load2); Serial.print(" "); 
+    Serial.print(load3); Serial.print(" "); 
+    Serial.println(load4);
+    digitalWrite(Relay1, load1); 
+    digitalWrite(Relay2, load2); 
+    digitalWrite(Relay3, load3); 
+    digitalWrite(Relay4, load4);
+    delay(300);
+    resetLoad();
 }
-
+void resetLoad(){
+  load1=0;load2=0;load3=0;load4=0;
+}
 void handle_OnConnect() {
   server.send(200, "text/html", SendHTML(load1,load2,load3,load4)); 
 }
 
-void handle_L1on()  {load1=0;data();}
-void handle_L1off() {load1=1;data();}
-
-void handle_L2on()  {load2=0;data();}
-void handle_L2off() {load2=1;data();}
-
-void handle_L3on()  {load3=0;data();}
-void handle_L3off() {load3=1;data();}
-
-void handle_L4on()  {load4=0;data();}
-void handle_L4off() {load4=1;data();}
+void handle_L1on()  {load1=1;data();}
+void handle_L2on()  {load2=1;data();}
+void handle_L3on()  {load3=1;data();}
+void handle_L4on()  {load4=1;data();}
 
 
 void data(){
@@ -110,6 +107,7 @@ EEPROM.write(2, load2);
 EEPROM.write(3, load3);
 EEPROM.write(4, load4);  
 EEPROM.commit();
+
   
 server.send(200, "text/html", SendHTML(load1,load2,load3,load4));   
 }
@@ -134,32 +132,19 @@ String SendHTML(uint8_t l1stat,uint8_t l2stat,uint8_t l3stat,uint8_t l4stat){
   ptr +="</style>\n";
   ptr +="</head>\n";
   ptr +="<body>\n";
-  ptr +="<h2>Hyundai Eon Lock Control</h2>\n";
+  ptr +="<h1>Hyundai-Eon Controller</h1>\n";
+ 
+
+  ptr +="<p>\n</p><a class=\"button button-on\" href=\"/L1on\">Lock</a>\n";
+  ptr +="<p> </p><a class=\"button button-on\" href=\"/L2on\">Unlock</a>\n";
+  ptr +="<p> </p><a class=\"button button-on\" href=\"/L3on\">Mute</a>\n";
+  ptr +="<p> </p><a class=\"button button-on\" href=\"/L4on\">Alarm</a>\n";
   
-   if(l1stat==0)
-  {ptr +="<p>Status: Unlock</p><a class=\"button button-off\" href=\"/L1off\">Lock</a>\n";}
-  else
-  {ptr +="<p>Status: Locked</p><a class=\"button button-on\" href=\"/L1on\">Unlock</a>\n";}
-
-  if(l2stat==0)
-  {ptr +="<p>Status: Mute</p><a class=\"button button-off\" href=\"/L2off\">Unmute</a>\n";}
-  else
-  {ptr +="<p>Status: Unmute</p><a class=\"button button-on\" href=\"/L2on\">Mute</a>\n";}
-
- /* if(l3stat==0)
-  {ptr +="<p>Load3 Status: ON</p><a class=\"button button-off\" href=\"/L3off\">OFF</a>\n";}
-  else
-  {ptr +="<p>Load3 Status: OFF</p><a class=\"button button-on\" href=\"/L3on\">ON</a>\n";}
-
-  if(l4stat==0)
-  {ptr +="<p>Load4 Status: ON</p><a class=\"button button-off\" href=\"/L4off\">OFF</a>\n";}
-  else
-  {ptr +="<p>Load4 Status: OFF</p><a class=\"button button-on\" href=\"/L4on\">ON</a>\n";}
-  */
+  
 
   ptr +="</body>\n";
   ptr +="<foot>\n";
-  ptr +="<p>By: Tashmin Mishra</p>\n";
+  ptr +="<p><i>By: Tashmin Mishra</i></p>\n";
   ptr +="</foot>\n";
   ptr +="</html>\n";
   return ptr;
